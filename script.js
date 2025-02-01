4d8bf66c2ab8af046dd5c13f137489963c07c3cb
Nabil657 <155356762+Nabil657@users.noreply.github.com>
2025-02-01 07:20:04 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    fetchPosts();
+
+    function fetchPosts() {
+        // ডাটাবেস থেকে তথ্য লোড করার জন্য এখানে API কল করা হবে
+        console.log("Loading posts...");
+    }
+});
